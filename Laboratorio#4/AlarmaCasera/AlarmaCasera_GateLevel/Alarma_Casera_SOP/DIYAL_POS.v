72ea0e70b01b4e173801fab0397550e4f79892f2
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-08-04 15:20:40 -0600

    Adicion de GTKwave

--- Laboratorio#4/AlarmaCasera/AlarmaCasera_GateLevel/Alarma_Casera_SOP/DIYAL_POS.v
+++ Laboratorio#4/AlarmaCasera/AlarmaCasera_GateLevel/Alarma_Casera_SOP/DIYAL_POS.v
@@ -1,5 +1,7 @@
 module DIYAL_POS();
-
+/*
+POS de la alarma de casa casera.
+*/
 reg S, W, M;
 wire NS, NW, NM, out, lin1, lin2, lin3;
 
@@ -11,6 +13,7 @@ and mult2(lin2, S, NW, M);
 and mult3(lin3, S, W, M);
 or sum1(out, lin1, lin2, lin3);
 
+//Para probar la salida que tendran las compuertas
   initial begin
     $display("S| W| M| A|");
     $display("-----------");
@@ -24,6 +27,12 @@ or sum1(out, lin1, lin2, lin3);
     #1 S=1; W=1; M=0;
     #1 S=1; W=1; M=1;
     #1 $finish;
+  end
+
+//utilizado para GTKwave
+  initial begin
+  $dumpfile("DIYAL_POS_tb.vcd");
+  $dumpvars(0,DIYAL_POS);
   end
 
 endmodule
