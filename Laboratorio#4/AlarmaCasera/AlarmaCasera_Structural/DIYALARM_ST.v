a12c92c6d601d41f3caec1d7579344736d10c816
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-08-04 21:05:20 -0600

    Creacion del testbench
    Primera prueba sin GTKwave

--- Laboratorio#4/AlarmaCasera/AlarmaCasera_Structural/DIYALARM_ST.v
+++ Laboratorio#4/AlarmaCasera/AlarmaCasera_Structural/DIYALARM_ST.v
@@ -1,7 +1,9 @@
 module DIYALARM_ST(
-  input wire S,W,K,
+  input wire S,W,M,
   output wire A);
-
+/*
+Se agregaron primero las entradas.
+*/
 assign A = S & (~W | M);
 
 endmodule
---
+++ Laboratorio#4/AlarmaCasera/AlarmaCasera_Structural/DIYALARM_ST_tb.v
@@ -0,0 +1,33 @@
+module testbench();
+/*
+TestBench para la alarma casera
+*/
+  reg S,M,W;
+  wire out;
+
+  DIYALARM_ST alm1(S,W,M,out);
+
+  initial begin
+    $display("S| W| M| A|");
+    $display("-----------");
+    $monitor("%b| %b| %b| %b|", S, W, M, out);
+    S=0; W=0; M=0;
+    #1 S=0; W=0; M=1;
+    #1 S=0; W=1; M=0;
+    #1 S=0; W=1; M=1;
+    #1 S=1; W=0; M=0;
+    #1 S=1; W=0; M=1;
+    #1 S=1; W=1; M=0;
+    #1 S=1; W=1; M=1;
+    #1 $finish;
+  end
+  // Parte para visualizar el GTKwave
+  /*
+  initial begin
+
+  $dumpfile("DIYALARM_ST_tb");
+  $dumpvars(0, testbench);
+
+  end
+  */
+endmodule
