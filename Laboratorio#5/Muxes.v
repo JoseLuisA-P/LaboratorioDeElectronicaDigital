5fd7284ff2ed273e29090029168620b069d26e44
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-08-13 15:18:38 -0600

    Tabla01
    adicion del mux 2:1 de la tabla 01

--- Laboratorio#5/Muxes.v
+++ Laboratorio#5/Muxes.v
@@ -10,14 +10,15 @@ module Mux2(
   output wire Y,
   input wire D0,D1,S);
 
-    Y = S ? D1:D0; //Si S=1 Y toma el valor de D1 Y si S=0 Y toma el valor de D0
+  assign  Y = S ? D1:D0; //Si S=1 Y toma el valor de D1 Y si S=0 Y toma el valor de D0
 
 endmodule
-
+//MUX 4:1 de uso general para la practica
 module Mux4(
   output wire Y,
   input wire D0,D1,D2,D3,S0,S1);
-  logic lin0, lin1;
+
+  logic lin0, lin1; //variables locales
 
   Mux2 selec0 (lin0, D0, D1, S0);
   Mux2 selec1 (lin1, D2, D3, S0);
@@ -25,4 +26,18 @@ module Mux4(
 
 endmodule
 
+////////////////////////////////////////////////////////////////////////////////
 //MUXES para el uso en las tablas
+//MUX 2:1 exclusivo a la tabla01 del lab5
+module MUX2T1(
+  output wire Y,
+  input wire A,B,C);
+
+  logic y1,y2; //variables locales
+
+  assign y1 = B ^ C; //xor
+  assign y2 = B ~^ C;//xnor
+
+  Mux2 select0 (Y, y1, y2, A);
+
+endmodule
