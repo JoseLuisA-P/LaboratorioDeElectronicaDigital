636e90e90904afa1c7186c114fc07682c26405be
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-08-13 15:31:53 -0600

    Adicion del MUX 2:1 TABLA 1
    Funciono como esperado el MUX2:1 de la tabla 1.

--- Laboratorio#5/Muxes.v
+++ Laboratorio#5/Muxes.v
@@ -4,6 +4,7 @@
 //Electronica digital
 // Muxes para el laboratorio 5
 ///////////////////////////////////////////////////////////////////////////////
+
 //MUXES de uso general
 //MUX 2:1 de uso general para la practica
 module Mux2(
@@ -13,12 +14,13 @@ module Mux2(
   assign  Y = S ? D1:D0; //Si S=1 Y toma el valor de D1 Y si S=0 Y toma el valor de D0
 
 endmodule
+
 //MUX 4:1 de uso general para la practica
 module Mux4(
   output wire Y,
   input wire D0,D1,D2,D3,S0,S1);
 
-  logic lin0, lin1; //variables locales
+  wire lin0, lin1; //variables locales
 
   Mux2 selec0 (lin0, D0, D1, S0);
   Mux2 selec1 (lin1, D2, D3, S0);
@@ -28,12 +30,13 @@ endmodule
 
 ////////////////////////////////////////////////////////////////////////////////
 //MUXES para el uso en las tablas
+
 //MUX 2:1 exclusivo a la tabla01 del lab5
 module MUX2T1(
   output wire Y,
   input wire A,B,C);
 
-  logic y1,y2; //variables locales
+  wire y1,y2; //variables locales
 
   assign y1 = B ^ C; //xor
   assign y2 = B ~^ C;//xnor
