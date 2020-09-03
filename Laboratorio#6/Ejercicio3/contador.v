ebe5c0d39279f7369e9cc93164fcb701045a1092
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-09-03 16:43:13 -0600

    Prueba del contador grey

--- Laboratorio#6/Ejercicio3/contador.v
+++ Laboratorio#6/Ejercicio3/contador.v
@@ -22,3 +22,16 @@ module Dflop(input logic clk,
 endmodule
 
 ////////////////////////////////////////////////////////////////////////////////
+module EJ3(input logic clk,
+  input logic reset,
+  input logic A,
+  output reg[2:0] sal);
+
+  wire[2:0] S;
+  Dflop FD1 (clk, reset, S, sal);
+  assign S[2] = (sal[2] & sal[0]) | (sal[1] & ~ sal[0] & A) | (~sal[1] & ~sal[0] & ~A);
+  assign S[1] = (sal[1] & ~sal[0]) | (~sal[2] & sal[0] & A) | (sal[2] & sal[0] & ~A);
+  assign S[0] = (sal[2] & sal[1] & A) | (~sal[2] & ~sal[1] & A) | (~sal[2] & sal[1] & ~A)
+                  | (sal[2] & ~sal[1] & ~A);
+                  
+endmodule
