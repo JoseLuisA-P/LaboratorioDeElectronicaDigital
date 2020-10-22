430b17021b713acd6e686101ca641523ff1873d9
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-10-22 15:17:10 -0600

    modificado ROM

--- Laboratorio#8/Memoria/ROM.v
+++ Laboratorio#8/Memoria/ROM.v
@@ -1,14 +1,14 @@
 module ROM (
-  input wire [11:0] direccion, //entrada de direccion, para seleccionar localidad
+  input wire [0:4095] direccion, //entrada de direccion, para seleccionar localidad
   output wire [7:0] salida //salida para dar los datos de localidad
   );
 
-//registro de 4K X 8
-reg [7:0] memoria[11:0];
+//registro de 4K X 8 (12 bits X 8 bits)
+reg [7:0] memoria[0:4095];
 
 //para leer la memoria en binario ante cada fila
 initial begin
-  $readmemb("memROM.list.txt", memoria);
+  $readmemb("MemROM.txt", memoria);
 end
 
 //asignar a la salida el valor de la memoria en esa localidad
