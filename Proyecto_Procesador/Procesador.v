fcf576252c71566023a53f7d63dbf44de44f4118
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-11-11 10:40:49 -0600

    borrador 1
    faltan los enables para cada modulo y la RAM

--- Proyecto_Procesador/Procesador.v
+++ Proyecto_Procesador/Procesador.v
@@ -22,11 +22,36 @@ wire C_FLAG,Z_FLAG,PHASE;
 wire[6:0] DECODE_address = {PHASE,C_FLAG,Z_FLAG,INSTR}
 wire[12:0] SEÑALES_DE_CONTROL;
 
-wire[3:0] ACCU;
+wire[3:0] ACCUU;
 wire[3:0] ALU_OUT;
+wire[3:0] DATA_BUS;
 wire ALUC,ALUZ;
 
 //Bloque DECODE
+ROMcase dicode(DECODE_address,SEÑALES_DE_CONTROL);
 
+//bloque de phase y de flags
+phase FASE(clock,reset,ENABLE,PHASE);//falta enable
+Flags banderas(clock,reset,ENABLE,{ALUC,ALUZ},{C_FLAG,Z_FLAG});//falta enable
+
+//bloque del ProgramCounter, programROM 64X8 y FETCH
+ProgramCounter contador(reset,clock,LOAD,ENABLE,address_ram,PC); //falta enable y load
+ROM opcode(PC,PROGRAM_BYTE);
+Fetch ftch(clock,reset,ENABLE,PROGRAM_BYTE,{INSTR,OPRND});//falta enable
+
+//bloque de la RAM
+
+//bloque del BusDriver conectado al fetch
+tribuff delfetch(ENABLE,OPRND,DATA_BUS);//falta enable
+
+//bloque del BusDriver conectado a la ALU
+tribuff deALU(ENABLE,ALU_OUT,DATA_BUS);//falta enable
+
+//bloque del BusDriver entradas de los pushbuttons
+tribuff pushes(ENABLE,pushbuttons,DATA_BUS);//falta enable
+
+//bloque de la ALU y el accu
+ALU operaciones(ACCUU,DATA_BUS,SELECTOR,ALU_OUT,ALUC,ALUZ);//falta selector
+ACCU acumulador(clock,reset,ENABLE,ALU_OUT,ACCUU);//falta enable
 
 endmodule
